552f91d6705a687bdd722ac90341a703c283188e
Lena <kencrimea@gmail.com>
2026-06-08 16:54:56 +0300

    installed Radix ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import "@radix-ui/themes/styles.css";
 import './App.css';
+import {Flex, Heading, Text} from "@radix-ui/themes";
 
 const App = () => {
-  return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
-  );
+    return (
+        <Flex height={'100dvh'} direction={'column'} align={'center'}
+              justify={'center'}>
+            <Heading as={'h1'}>List management</Heading>
+            <Text>Stop forgetting things. Create lists.</Text>
+        </Flex>
+    );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import {Theme} from "@radix-ui/themes";
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+    const root = ReactDOM.createRoot(rootEl);
+    root.render(
+        <React.StrictMode>
+            <Theme><App/></Theme>
+        </React.StrictMode>,
+    );
 }
